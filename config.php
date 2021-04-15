14996711150bbb7446f9bf1420ebccfce34e90ed
Raphael Serafim <68257896+raphaelvserafim@users.noreply.github.com>
2021-04-15 17:24:14 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+date_default_timezone_set('America/Cuiaba');
+
+ 
+// CONFIG DB 
+define("DB_HOST", "localhost");
+define("DB_BASE",  "");
+define("DB_USER",  "");
+define("DB_SENHA", "");
+
+ 
