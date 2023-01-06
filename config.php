4d9d132351a8532b944e3b87768cafcd865ac325
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-06 10:39:45 -0400

    require 'vendor/autoload.php';

--- config.php
+++ config.php
@@ -3,12 +3,13 @@
 date_default_timezone_set('America/Cuiaba');
 
  
+
+
 // CONFIG DB 
 define("DB_HOST", "localhost");
 define("DB_BASE",  "");
 define("DB_USER",  "");
 define("DB_SENHA", "");
 
- 
-require   'classe/db.php';
-require   'classe/sicoob.php';
+
+require 'vendor/autoload.php'; 
